0a508d76ef46573491bd93f201bd128f25221aa5
samuel petersen <samuelkpeter@gmail.com>
2021-07-27 20:09:04 -0300

    Home controller documentation

--- Application/Controllers/Home.php
+++ Application/Controllers/Home.php
@@ -8,11 +8,11 @@ Class Home extends Authenticated {
     /**
      * When user logs in the example routes user to /home/dashboard.
      * 
-     * the get method on superclass calls showViewBasedOnRoute() that open /Application/Views/{CrontrollerName}/{ActionName}
+     * the @GET method on main Controller class calls showViewBasedOnRoute() that opens /Application/Views/{CrontrollerName}/{ActionName}
      * 
-     * If no actionhame is supplied the default action name is index.
+     * If no action name is supplied, is index.
      * 
-     * As you can see, if the content only show a view based on route, there is no need to have code inside the controller
+     * So for this homepage controller, the Controller class already handles a simple and basic call for a main index page. No extra coding needs to be done!
      * 
      */
     
